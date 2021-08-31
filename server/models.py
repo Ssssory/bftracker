c3093b97c0697bccdb0ddaeaffac2f75072d4005
Ssssory <itrubchaninov@list.ru>
2021-09-01 00:24:27 +0300

    server models modified

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -10,7 +11,18 @@ class Restaurant(models.Model):
     ul = models.CharField(max_length=200, blank=True)
     inn = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    closed_at = models.DateTimeField(blank=True)
+    closed_at = models.DateTimeField(auto_now_add=True)
+
+    def as_json(self):
+        return dict(
+            id=self.pk,
+            login=self.login, 
+            email=self.email,
+            name=self.name,
+            city=self.city,
+            ul=self.ul,
+            inn=self.inn,
+            created_at=self.created_at)
 
 class Point(models.Model):
     restaurant = models.ForeignKey(Restaurant, related_name="point", on_delete=models.CASCADE)
@@ -19,7 +31,14 @@ class Point(models.Model):
     login = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
-    closed_at = models.DateTimeField(blank=True)
+    closed_at = models.DateTimeField(auto_now_add=True)
+    def as_json(self):
+        return dict(
+            id=self.pk,
+            login=self.login,
+            address=self.address,
+            name=self.name,
+            created_at=self.created_at.strftime(settings.DATE_FORMAT))
 
 
 class Client(models.Model):
@@ -31,16 +50,25 @@ class Client(models.Model):
 class Order(models.Model):
     external_id = models.CharField(max_length=50, blank=True)
     order_id = models.CharField(max_length=30)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    client = models.ForeignKey(
+        Client, on_delete=models.CASCADE, blank=True, null=True)
     point = models.ForeignKey(Point, on_delete=models.CASCADE)
-    status_id = models.PositiveIntegerField()
+    status = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def as_json(self):
+        return dict(
+            id=self.pk,
+            order_id=self.order_id,
+            external_id=self.external_id,
+            status=self.status,
+            created_at=self.created_at.strftime(settings.DATE_FORMAT))
 
 
 class OrderStatusesHistory(models.Model):
     order = models.ForeignKey(Order,related_name="status_history", on_delete=models.CASCADE)
-    status_id = models.PositiveIntegerField()
+    status = models.CharField(max_length=30)
     date_start = models.DateTimeField()
     duration = models.PositiveIntegerField()
 
